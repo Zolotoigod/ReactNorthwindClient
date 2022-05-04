edea09c2566dd0d11833f8aaa00f2564828b2014
vladshalkevich@gmail.com <vladshalkevich@gmail.com>
2022-05-04 21:47:18 +0300

    initial

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Home extends Component {
+    
+    render(){
+        return(
+            <div>
+                <h2>NorthWindClient</h2>
+            </div>
+        )
+    }
+}
+
+export default Home
