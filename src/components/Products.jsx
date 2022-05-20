e1f28e31cd491ace5ea1ed5bde94a7520c680711
vladshalkevich@gmail.com <vladshalkevich@gmail.com>
2022-05-20 20:22:17 +0300

    demo

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import { Table } from 'react-bootstrap';
+import NumberFormat from 'react-number-format';
+import Pages from './Pages';
 
 class Products extends Component {
     constructor(props){
@@ -7,31 +9,44 @@ class Products extends Component {
         this.state={prods:[]}
     }
     componentDidMount(){
-        this.setList();
+        this.setList(this.props);
     }
-    setList(){
-
+    setList(props){
+        let currentPage = 0;
+        const productPath = `https://localhost:5001/api/products/${currentPage * 10}/10`;
+        fetch(productPath)
+        .then(response => response.json())
+        .then(data => {
+            this.setState({prods:data})
+        })
     }
     render(){
         const {prods} = this.state;
         return(
             <div>
-                <Table className='mt-4' striped bordered hover size='lg'>
+                <Table className='mt-4' striped bordered hover variant='dark' size='lg'>
                     <thead>
                         <tr>
                             <th>Product ID</th>
                             <th>Product Name</th>
+                            <th>Quantity Per Unit</th>
+                            <th>Unit Price</th>
+                            <th>Units In Stock</th>
                         </tr>
                     </thead>
                     <tbody>
                         {prods.map(product=>
-                                <tr key={product.ProductId}>
-                                    <td>{product.ProductId}</td>
-                                    <td>{product.ProductName}</td>
+                                <tr key={product.productId}>
+                                    <td>{product.productId}</td>
+                                    <td className='text-justify'>{product.productName}</td>
+                                    <td className='text-justify'>{product.quantityPerUnit}</td>
+                                    <td><NumberFormat displayType='text' value={product.unitPrice} suffix={'$'}/></td>
+                                    <td className='text-end'>{product.unitsInStock}</td>
                                 </tr>
                             )}
                     </tbody>
                 </Table>
+                <Pages/>
             </div>
         )
     }
